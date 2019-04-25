22306c81c5dba7bae63277b51f075020646747a1
Jared Glaser <Jared@glaserfamily.net>
2019-04-25 19:09:12 -0400

    Adding result object

---
+++ src/csvdemo/Result.java
@@ -0,0 +1,18 @@
+package csvdemo;
+
+public class Result {
+		private int numReps;
+		
+		public Result(int numReps) {
+			this.numReps = numReps;
+		}
+		
+		public int getNumReps() {
+			return numReps;
+		}
+
+		public void setNumReps(int numReps) {
+			this.numReps = numReps;
+		}
+	
+}
